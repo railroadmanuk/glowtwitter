61948e8d400e2b6374de93bc129efd9b0e814173
Tim Hynes <mrtimhynes@googlemail.com>
2017-06-07 19:02:39 +0100

    Added image

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This project is to combine the Twitter API with the PiGlow Raspberry Pi accessory. The idea being that when a Twitter notification is received, the PiGlow will pulse to notify me.
 ## Dependencies
 This project relies on the PyGlow Python library, available at: https://github.com/benleb/PyGlow
+
+![](logo.png)
--- glow_tweet.py
+++ glow_tweet.py
@@ -12,14 +12,15 @@ pyglow = PyGlow()
 pyglow.all(0)
 
 def notify_glow(num_pulses, in_colours):
-    for i in range(num_pulses):
-      for colour in in_colours:
-        pyglow.color(colour, 100)
-        sleep(0.1)
-        pyglow.all(0)
+  for i in range(num_pulses):
+    for colour in in_colours:
+      pyglow.color(colour, 100)
+      sleep(0.1)
+      pyglow.all(0)
 
 def main():
-    notify_glow(5,all_colours)
+  # Test glow with all colours
+  notify_glow(5,all_colours)
 
 if __name__ == "__main__":
-    main()
+  main()
