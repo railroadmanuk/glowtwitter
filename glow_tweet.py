c2b174a8dcff46996aff470c732d8c825e0f82ee
Tim Hynes <mrtimhynes@googlemail.com>
2017-06-07 18:47:38 +0100

    Made pulsing a function

--- glow_tweet.py
+++ glow_tweet.py
@@ -3,14 +3,23 @@
 from PyGlow import PyGlow
 from time import sleep
 from datetime import datetime
-
-pyglow = PyGlow()
-pyglow.all(0)
-
+# List all colours
 all_colours = ["white","blue","green","yellow","orange","red"]
 
-for i in range(5): 
-  for colour in all_colours:
-    pyglow.color(colour, 100)
-    sleep(0.1)
-    pyglow.all(0)
+# instantiate the PyGlow API
+pyglow = PyGlow()
+# Clear the current status
+pyglow.all(0)
+
+def notify_glow(num_pulses, in_colours):
+    for i in range(num_pulses):
+      for colour in in_colours:
+        pyglow.color(colour, 100)
+        sleep(0.1)
+        pyglow.all(0)
+
+def main():
+    notify_glow(5,all_colours)
+
+if __name__ == "__main__":
+    main()
